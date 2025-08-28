e7f743c698517c253e125831a0b5a9717afab0bd
beemdvp <ibrahimpmahmood@gmail.com>
2025-08-28 15:32:50 +0100

    feat: update incentives api shape

--- src/radixIncentives.ts
+++ src/radixIncentives.ts
@@ -1,32 +1,51 @@
-export interface ActivityCategory {
+// export interface ActivityCategory {
+//     id: string
+//     name: string
+//     description: string | null
+// }
+
+// export interface ActivityItem {
+//     id: string
+//     name: string | null
+//     description: string | null
+//     category: string
+//     dapp: string
+//     componentAddresses: string[]
+//     data: Record<string, unknown>
+//     activityCategories: ActivityCategory
+// }
+
+// export interface ActivityData {
+//     provideXrdDerivativeLiquidityToDex: ActivityItem[]
+//     tradingVolume: ActivityItem[]
+//     maintainXrdBalance: ActivityItem[]
+//     provideBlueChipLiquidityToDex: ActivityItem[]
+//     provideStablesLiquidityToDex: ActivityItem[]
+//     provideNativeLiquidityToDex: ActivityItem[]
+//     lendingXrdDerivative: ActivityItem[]
+// }
+
+export interface Activity {
     id: string
     name: string
-    description: string | null
-}
-
-export interface ActivityItem {
-    id: string
-    name: string | null
     description: string | null
     category: string
     dapp: string
     componentAddresses: string[]
-    data: Record<string, unknown>
-    activityCategories: ActivityCategory
-}
-
-export interface ActivityData {
-    provideXrdDerivativeLiquidityToDex: ActivityItem[]
-    tradingVolume: ActivityItem[]
-    maintainXrdBalance: ActivityItem[]
-    provideBlueChipLiquidityToDex: ActivityItem[]
-    provideStablesLiquidityToDex: ActivityItem[]
-    provideNativeLiquidityToDex: ActivityItem[]
-    lendingXrdDerivative: ActivityItem[]
+    data: {
+        ap: boolean
+        multiplier: boolean
+        showOnEarnPage: boolean
+    }
+    activityCategories: {
+        id: string
+        name: string
+        description: string | null
+    }
 }
 
 export type RadixIncentivesResponse = [
-    { result: { data: { json: { groupedByCategory: ActivityData } } } },
+    { result: { data: { json: Activity[] } } },
 ]
 
 export const getRadixIncentives = async (category: 'liquidity' | 'lending') => {
@@ -35,16 +54,16 @@ export const getRadixIncentives = async (category: 'liquidity' | 'lending') => {
     )
         .then((res) => res.json() as Promise<RadixIncentivesResponse>)
         .then((res) => {
-            const activityData = res[0].result.data.json.groupedByCategory
+            const activityData = res[0].result.data.json
 
-            return Object.keys(activityData).reduce((acc, key) => {
-                if (key.toLowerCase().includes(category)) {
-                    activityData[key as keyof typeof activityData]
-                        .map((ad) => ad.componentAddresses)
-                        .flat()
-                        .forEach((address) => {
-                            acc.add(address)
-                        })
+            return activityData.reduce((acc, key) => {
+                if (
+                    key.category.toLowerCase().includes(category) &&
+                    key.data.ap
+                ) {
+                    key.componentAddresses.forEach((address) => {
+                        acc.add(address)
+                    })
                     return acc
                 }
                 return acc
